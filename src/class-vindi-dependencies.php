cb86b106f0ee3db09ce270249ab5e04072025e6a
Erico Pedroso <erico.pedroso@vindi.com.br>
2015-10-28 20:39:00 -0200

    learn how flows

--- src/class-vindi-dependencies.php
+++ src/class-vindi-dependencies.php
@@ -2,59 +2,61 @@
 
 class Vindi_Dependencies
 {
+    /**
+     * @var array
+     **/
+    private static $active_plugins;
 
-	private static $active_plugins;
+    /**
+    * Init Vindi_Dependencies.
+    */
+    public static function init()
+    {
+        self::$active_plugins = (array) get_option( 'active_plugins', array() );
 
-  /**
-   * Init Vindi_Dependencies.
-   */
-	public static function init()
-  {
-		self::$active_plugins = (array) get_option( 'active_plugins', array() );
+        if ( is_multisite() )
+        self::$active_plugins = array_merge( self::$active_plugins, get_site_option( 'active_sitewide_plugins', array() ) );
+    }
 
-		if ( is_multisite() )
-			self::$active_plugins = array_merge( self::$active_plugins, get_site_option( 'active_sitewide_plugins', array() ) );
-	}
+    /**
+    * WooCommerce fallback notice.
+    * @return  boolean
+    */
+    public static function check()
+    {
+        $plugin = strtolower($plugin);
 
-  /**
-   * WooCommerce fallback notice.
-   * @return  boolean
-   */
-	public static function check()
-  {
-		$plugin = strtolower($plugin);
+        if ( ! self::$active_plugins ) self::init();
 
-		if ( ! self::$active_plugins ) self::init();
+        if ( ! self::is_woocommerce_activated() ) {
+            add_action( 'admin_notices', 'Vindi_Dependencies::woocommerceMissingNotice' );
+            return false;
+        }
+    }
 
-		if ( ! self::is_woocommerce_activated() ) {
-			add_action( 'admin_notices', 'Vindi_Dependencies::woocommerceMissingNotice' );
-			return false;
-		}
-	}
+    /**
+    * WooCommerce fallback notice.
+    * @return  string
+    */
+    public static function woocommerceMissingNotice()
+    {
+        echo '<div class="error"><p>' . sprintf( __( 'WooCommerce Vindi Gateway depende da última versão do %s para funcionar!', VINDI_IDENTIFIER ), '<a href="https://wordpress.org/extend/plugins/woocommerce/">' . __( 'WooCommerce', VINDI_IDENTIFIER ) . '</a>' ) . '</p></div>';
+    }
 
-  /**
-   * WooCommerce fallback notice.
-   * @return  string
-   */
-  public static function woocommerceMissingNotice()
-  {
-      echo '<div class="error"><p>' . sprintf( __( 'WooCommerce Vindi Gateway depende da última versão do %s para funcionar!', VINDI_IDENTIFIER ), '<a href="https://wordpress.org/extend/plugins/woocommerce/">' . __( 'WooCommerce', VINDI_IDENTIFIER ) . '</a>' ) . '</p></div>';
-  }
+    /**
+    * WooCommerce Extra Checkout Fields for Brazil fallback notice.
+    * @return  string
+    */
+    public static function extraCheckoutMissingNotice()
+    {
+        echo '<div class="error"><p>' . sprintf( __( 'WooCommerce Vindi Gateway depende da última versão do %s para funcionar!', VINDI_IDENTIFIER ), '<a href="https://wordpress.org/extend/plugins/woocommerce-extra-checkout-fields-for-brazil/">' . __( 'WooCommerce Extra Checkout Fields for Brazil', VINDI_IDENTIFIER ) . '</a>' ) . '</p></div>';
+    }
 
-  /**
-   * WooCommerce Extra Checkout Fields for Brazil fallback notice.
-   * @return  string
-   */
-  public static function extraCheckoutMissingNotice()
-  {
-    echo '<div class="error"><p>' . sprintf( __( 'WooCommerce Vindi Gateway depende da última versão do %s para funcionar!', VINDI_IDENTIFIER ), '<a href="https://wordpress.org/extend/plugins/woocommerce-extra-checkout-fields-for-brazil/">' . __( 'WooCommerce Extra Checkout Fields for Brazil', VINDI_IDENTIFIER ) . '</a>' ) . '</p></div>';
-  }
-
-	/**
-	 * @return boolean
-	 **/
-	public function is_woocommerce_activated()
-	{
-		return in_array( 'woocommerce/woocommerce.php', self::$active_plugins ) || array_key_exists( 'woocommerce/woocommerce.php', self::$active_plugins );
-	}
+    /**
+    * @return boolean
+    **/
+    public function is_woocommerce_activated()
+    {
+        return in_array( 'woocommerce/woocommerce.php', self::$active_plugins ) || array_key_exists( 'woocommerce/woocommerce.php', self::$active_plugins );
+    }
 }
--- vindi-woocommerce-subscriptions.php
+++ vindi-woocommerce-subscriptions.php
@@ -1,21 +1,21 @@
 <?php
 /**
- * Plugin Name: Vindi Woocommerce Subscriptions
- * Plugin URI:
- * Description: Adiciona o gateway de pagamentos da Vindi para o WooCommerce Subscriptions.
- * Version: 1.0.0
- * Author: Vindi
- * Author URI: https://www.vindi.com.br
- * Requires at least: 4.0
- * Tested up to: 4.2
- *
- * Text Domain: vindi-woocommerce-subscriptions
- * Domain Path: /languages/
- *
- * Copyright: © 2014-2015 Vindi Tecnologia e Marketing LTDA
- * License: GPLv3 or later
- * License URI: http://www.gnu.org/licenses/gpl-3.0.html
- */
+* Plugin Name: Vindi Woocommerce Subscriptions
+* Plugin URI:
+* Description: Adiciona o gateway de pagamentos da Vindi para o WooCommerce Subscriptions.
+* Version: 1.0.0
+* Author: Vindi
+* Author URI: https://www.vindi.com.br
+* Requires at least: 4.0
+* Tested up to: 4.2
+*
+* Text Domain: vindi-woocommerce-subscriptions
+* Domain Path: /languages/
+*
+* Copyright: © 2014-2015 Vindi Tecnologia e Marketing LTDA
+* License: GPLv3 or later
+* License URI: http://www.gnu.org/licenses/gpl-3.0.html
+*/
 
 if ( ! defined( 'ABSPATH' ) ) die( 'No script kiddies please!' );
 
@@ -24,74 +24,107 @@ define('VINDI_IDENTIFIER', 'vindi_subscriptions');
 require_once dirname(__FILE__)."/src/class-vindi-dependencies.php";
 
 /**
- * Check all Vindi Dependencies
- */
+* Check all Vindi Dependencies
+*/
 if( false === Vindi_Dependencies::check()) {
 	return ;
 }
 
 if ( ! class_exists( 'Vindi_WooCommerce_Subscriptions' ) )
 {
-    class Vindi_WooCommerce_Subscriptions
-    {
-        /**
-         * @var string
-         */
-        CONST VERSION = '1.0.0';
+	class Vindi_WooCommerce_Subscriptions
+	{
+	    /**
+		 * @var string
+		 */
+		CONST VERSION = '1.0.0';
 
-				/**
-         * @var string
-         */
-        CONST VIEWS_DIR = 'views';
+		/**
+		 * @var string
+		 */
+		CONST VIEWS_DIR = 'views';
 
-        /**
-         * Instance of this class.
-         * @var WCS_Vindi
-         */
-        protected static $instance = null;
+		/**
+		 * Instance of this class.
+		 * @var WCS_Vindi
+		 */
+		protected static $instance = null;
 
-        /**
-         * Set up the class, including it's hooks & filters, when the file is loaded.
-         **/
-        public static function init()
-        {
-            $GLOBALS[VINDI_IDENTIFIER] = self::get_instance();
+		/**
+		 * Set up the class, including it's hooks & filters, when the file is loaded.
+		 **/
+		public function __construct()
+		{
+			add_filter('woocommerce_settings_tabs_array', [&$this, 'add_settings_tab'], 100);
+			add_action('woocommerce_settings_tabs_'.VINDI_IDENTIFIER, [&$this, 'settings_tab']);
+		}
 
-						add_filter( 'woocommerce_settings_tabs_array', __CLASS__ . '::add_settings_tab', 100 );
-						add_action( 'woocommerce_settings_tabs_'.VINDI_IDENTIFIER, __CLASS__ . '::load_settings_tab' );
-        }
+		/**
+		 * Return an instance of this class.
+		 * @return WCS_Vindi A single instance of this class.
+		 */
+		public static function get_instance()
+		{
+			// If the single instance hasn't been set, set it now.
+			if ( null === self::$instance )
+			self::$instance = new self;
 
-        /**
-         * Return an instance of this class.
-         * @return WCS_Vindi A single instance of this class.
-         */
-        public static function get_instance()
-        {
-            // If the single instance hasn't been set, set it now.
-            if ( null === self::$instance )
-                 self::$instance = new self;
+			return self::$instance;
+		}
 
-            return self::$instance;
-        }
+		/**
+		 * Create a tab settings at woocommerce tabs
+		 */
+		public function add_settings_tab( $settings_tabs )
+		{
+			$settings_tabs[VINDI_IDENTIFIER] = __('Vindi', 'vindi-settings');
 
-				/**
-				 * Create a tab settings at woocommerce tabs
-				 */
-				public function add_settings_tab( $settings_tabs )
-				{
-					$settings_tabs[VINDI_IDENTIFIER] = __( 'Vindi', 'vindi-settings' );
+			return $settings_tabs;
+		}
 
-	        return $settings_tabs;
-				}
+		/**
+		 * Check if SSL is enabled when merchant is not trial.
+		 * @return boolean
+		 */
+		protected function checkSsl()
+		{
+			return false;
+		}
 
-				/**
-				 * Create settings fields in Vind settings tab
-				 **/
-				public function load_settings_tab()
-				{
-					include_once(self::VIEWS_DIR.'/admin-settings.html.php');
-				}
-    }
+		public function settings_tab()
+		{
+		    require_once(self::VIEWS_DIR.'/admin-settings.html.php');
+		}
+
+		function generate_settings_html()
+		{
+		    $settings = array(
+		        'section_title' => array(
+		            'name'     => __( 'Section Title', 'woocommerce-settings-tab-demo' ),
+		            'type'     => 'title',
+		            'desc'     => '',
+		            'id'       => 'wc_settings_tab_demo_section_title'
+		        ),
+		        'title' => array(
+		            'name' => __( 'Title', 'woocommerce-settings-tab-demo' ),
+		            'type' => 'text',
+		            'desc' => __( 'This is some helper text', 'woocommerce-settings-tab-demo' ),
+		            'id'   => 'wc_settings_tab_demo_title'
+		        ),
+		        'description' => array(
+		            'name' => __( 'Description', 'woocommerce-settings-tab-demo' ),
+		            'type' => 'textarea',
+		            'desc' => __( 'This is a paragraph describing the setting. Lorem ipsum yadda yadda yadda. Lorem ipsum yadda yadda yadda. Lorem ipsum yadda yadda yadda. Lorem ipsum yadda yadda yadda.', 'woocommerce-settings-tab-demo' ),
+		            'id'   => 'wc_settings_tab_demo_description'
+		        ),
+		        'section_end' => array(
+		             'type' => 'sectionend',
+		             'id' => 'wc_settings_tab_demo_section_end'
+		        )
+		    );
+		    return woocommerce_admin_fields(apply_filters( 'wc_settings_tab_vindi_settings', $settings ));
+		}
+	}
 }
 
-Vindi_WooCommerce_Subscriptions::init();
+add_action( 'admin_init', [ 'Vindi_WooCommerce_Subscriptions', 'get_instance' ], 100 );
