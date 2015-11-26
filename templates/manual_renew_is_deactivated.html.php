a59fb58568c8316f02675549f00c35f950091d0a
Erico Pedroso <erico.pedroso@vindi.com.br>
2015-11-26 17:29:12 -0200

    fix warning message

--- templates/manual_renew_is_deactivated.html.php
+++ templates/manual_renew_is_deactivated.html.php
@@ -1,4 +1,4 @@
 <div class="error notice">
 	<h3><?php echo __('Renovação Automática de Assinaturas', VINDI_IDENTIFIER );?></h3>
-	<p><?php echo __('Para que a Vindi cuide das renavações das assinaturas você precisa habilidar as opções "' . __('Accept Manual Renewals', 'woocommerce-subscriptions') . '" e "' . __('Turn off Automatic Payments', 'woocommerce-subscriptions') . '" nas configurações do WooCommerce Subscriptions', VINDI_IDENTIFIER); ?></p>
+	<p><?php echo __('Para que a Vindi cuide das renovações das assinaturas você precisa habilidar as opções "' . __('Accept Manual Renewals', 'woocommerce-subscriptions') . '" e "' . __('Turn off Automatic Payments', 'woocommerce-subscriptions') . '" nas configurações do WooCommerce Subscriptions', VINDI_IDENTIFIER); ?></p>
 </div>
