77a8ed36796979307fcc23b76e1d5edf9545addf
Erico Pedroso <erico.pedroso@vindi.com.br>
2015-12-02 17:42:32 -0200

    fix period_created webhook

--- includes/class-vindi-webhook-handler.php
+++ includes/class-vindi-webhook-handler.php
@@ -100,7 +100,7 @@ class Vindi_Webhook_Handler
         $cycle           = $data->period->cycle;
         $subscription    = $this->find_subscription_by_id($data->period->subscription->code);
 
-        if($this->find_order_by_cycle($subscription->id, $cycle))
+        if($this->subscription_has_order_in_cycle($subscription->id, $cycle))
             throw new Exception('Já existe o ciclo $cycle para a assinatura ' . $data->period->subscription->id . ' pedido ' . $subscription->id);
 
         WC_Subscriptions_Manager::prepare_renewal($subscription->id);
@@ -120,7 +120,7 @@ class Vindi_Webhook_Handler
             $wc_subscription_id    = $data->bill->subscription->code;
             $vindi_subscription_id = $data->bill->subscription->id;
             $cycle                 = $data->bill->period->cycle;
-            $order                 = $this->find_order_by_cycle($vindi_subscription_id, $cycle);
+            $order                 = $this->find_order_by_subscription_and_cycle($vindi_subscription_id, $cycle);
 
             if(!$order)
                 throw new Exception('Não existe o ciclo $cycle para a assinatura ' . $data->period->subscription->id . ' pedido ' . $wc_subscription_id, 2);
@@ -142,9 +142,9 @@ class Vindi_Webhook_Handler
             $vindi_subscription_id = $data->bill->subscription->id;
             $cycle                 = $data->bill->period->cycle;
             $subscription          = $this->find_subscription_by_id($wc_subscription_id);
-            $order                 = $this->find_order_by_cycle($vindi_subscription_id, $cycle);
+            $order                 = $this->find_order_by_subscription_and_cycle($vindi_subscription_id, $cycle);
         }
-        
+
         $order->payment_complete();
         $order->add_order_note('Nova confirmação de pagamento!');
     }
@@ -156,7 +156,7 @@ class Vindi_Webhook_Handler
     private function charge_rejected($data)
     {
         $order = $this->find_order_by_bill_id($data->charge->bill->id);
-        
+
         if($order->get_status() == 'pending'){
             $order->update_status('failed', 'Pagamento rejeitado!');
         }else{
@@ -238,28 +238,60 @@ class Vindi_Webhook_Handler
 	 *
 	 * @return WC_Order
 	 */
-	private function find_order_by_cycle($subscription_id, $cycle)
+	private function find_order_by_subscription_and_cycle($subscription_id, $cycle)
     {
-		$args = array(
-			'post_type'   => 'shop_order',
-            'meta_query' => array(
-                array(
-        			'key'    => 'vindi_wc_cycle',
-        			'value'  => $cycle,
-                ),
-                array(
-        			'key'    => 'vindi_wc_subscription_id',
-        			'value'  => $subscription_id,
-                )
+        $query = $this->query_order_by_metas(array(
+            array(
+                'key'    => 'vindi_wc_cycle',
+                'value'  => $cycle,
             ),
-			'post_status' => 'any',
-		);
-
-        $query = new WP_Query($args);
+            array(
+                'key'    => 'vindi_wc_subscription_id',
+                'value'  => $subscription_id,
+            )
+        ));
 
         if(false === $query->have_posts())
             throw new Exception('Pedido da assinatura #' . $subscription_id . ' para o ciclo #' . $cycle . ' não encontrada!', 2);
 
         return wc_get_order($query->post->ID);
+	}
+
+    /**
+	 * @param int $subscription_id
+	 * @param int $cycle
+     *
+     * @return boolean
+	 */
+	private function subscription_has_order_in_cycle($subscription_id, $cycle)
+    {
+        $query = $this->query_order_by_metas(array(
+            array(
+                'key'    => 'vindi_wc_cycle',
+                'value'  => $cycle,
+            ),
+            array(
+                'key'    => 'vindi_wc_subscription_id',
+                'value'  => $subscription_id,
+            )
+        ));
+
+        return $query->have_posts();
+	}
+
+    /**
+	 * @param array $metas
+     *
+     * @return WP_Query
+	 */
+	private function query_order_by_metas(array $metas)
+    {
+        $args = array(
+			'post_type'   => 'shop_order',
+            'meta_query'  => $metas,
+			'post_status' => 'any',
+		);
+
+        return new WP_Query($args);
 	}
 }
