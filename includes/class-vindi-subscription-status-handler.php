9941aee27199822055ac54755b0516f26ad2d716
Erico Pedroso <erico.pedroso@vindi.com.br>
2015-12-02 19:30:46 -0200

    filter pending-cancel status to cancelled

--- includes/class-vindi-subscription-status-handler.php
+++ includes/class-vindi-subscription-status-handler.php
@@ -14,6 +14,10 @@ class Vindi_Subscription_Status_Handler
         add_action('woocommerce_subscription_status_cancelled',array(
             &$this, 'cancelled_subscription'
         ));
+
+        add_action('woocommerce_subscription_status_updated',array(
+            &$this, 'filter_pre_cancelled_status'
+        ), 1, 2);
     }
 
     /**
@@ -25,5 +29,15 @@ class Vindi_Subscription_Status_Handler
         $vindi_subscription_id      = end($vindi_subscription_id_meta);
 
         $this->container->api->delete_subscription($vindi_subscription_id, true);
+    }
+
+    /**
+     * @param WC_Subscription $wc_subscription
+     * @param string          $new_status
+     **/
+    public function filter_pre_cancelled_status($wc_subscription, $new_status)
+    {
+        if('pending-cancel' === $new_status)
+            $this->update_status('cancelled');
     }
 }
