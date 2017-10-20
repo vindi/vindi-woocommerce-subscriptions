d790e6320d5224893c6f9fea98ce53a48c781b92
lyoncesar <lyoncesar@gmail.com>
2017-10-20 14:39:59 -0200

    ajuste no parametro da funcao get_vindi_subscription_id

--- includes/class-vindi-subscription-status-handler.php
+++ includes/class-vindi-subscription-status-handler.php
@@ -25,7 +25,7 @@ class Vindi_Subscription_Status_Handler
      **/
     public function cancelled_subscription($wc_subscription)
     {
-        $this->container->api->delete_subscription($this->get_vindi_subscription_id($wc_subscription->id), true);
+        $this->container->api->delete_subscription($this->get_vindi_subscription_id($wc_subscription), true);
     }
 
     /**
@@ -40,7 +40,7 @@ class Vindi_Subscription_Status_Handler
             if(false == $memberships) {
                 $wc_subscription->update_status('cancelled');
             } else {
-                $this->container->api->delete_subscription($this->get_vindi_subscription_id($wc_subscription->id), true);
+                $this->container->api->delete_subscription($this->get_vindi_subscription_id($wc_subscription), true);
             }
         }
     }
@@ -48,8 +48,8 @@ class Vindi_Subscription_Status_Handler
     /**
      * @param WC_Subscription $wc_subscription
      **/
-    public function get_vindi_subscription_id($wc_subscription_id)
+    public function get_vindi_subscription_id($wc_subscription)
     {
-        return end(get_post_meta($wc_subscription_id, 'vindi_wc_subscription_id'));
+        return end(get_post_meta($wc_subscription->id, 'vindi_wc_subscription_id'));
     }
 }
