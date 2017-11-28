8b360493243e7fc043420218278961b839286065
laerte-guimaraes <31661772+laerte-guimaraes@users.noreply.github.com>
2017-11-28 19:32:16 -0200

    Alteração na captura do valor do produto
     Atualmente o plugin da Vindi utiliza o valor do cadastro do produto e não o valor do Carrinho/Checkout. Esse PR foi criado para viabilizar Integrações com plugins que realizam alteração em valores de produto.

--- includes/class-vindi-payment.php
+++ includes/class-vindi-payment.php
@@ -396,7 +396,7 @@ class Vindi_Payment
             $product                       = $this->get_product($order_item);
             $order_items[$key]['type']     = 'product';
             $order_items[$key]['vindi_id'] = $product->vindi_id;
-            $order_items[$key]['price']    = (float) $product->get_price();
+            $order_items[$key]['price']    = $order_item['total'];
         }
 
         return $order_items;
