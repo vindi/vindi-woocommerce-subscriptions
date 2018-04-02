9429b2455a5a23829c0f8ffae9eff772eda64137
lyoncesar <lyoncesar@gmail.com>
2018-04-02 19:33:13 -0300

    versionamento

--- vindi-woocommerce-subscriptions.php
+++ vindi-woocommerce-subscriptions.php
@@ -7,7 +7,7 @@
 * Author: Vindi
 * Author URI: https://www.vindi.com.br
 * Requires at least: 4.4
-* Tested up to: 4.9.1
+* Tested up to: 4.9.2
 *
 * Text Domain: vindi-woocommerce-subscriptions
 * Domain Path: /languages/
@@ -37,7 +37,7 @@ if (! class_exists('Vindi_WooCommerce_Subscriptions'))
 	    /**
 		 * @var string
 		 */
-		const VERSION = '3.0.7';
+		const VERSION = '3.0.8';
 
         /**
 		 * @var string
