dbcb4375318115f940e6dcd0d4228222ec83adba
Unknown <laerte.guimaraes@vindi.com.br>
2018-06-15 11:46:17 -0300

    Ajusta mensagem de erro quando o HTTPS não está ativado

--- templates/admin-settings.html.php
+++ templates/admin-settings.html.php
@@ -3,8 +3,7 @@
 <?php if (! $settings->check_ssl()): ?>
 <div class="error">
     <p>
-            <strong><?php _e('Vindi WooCommerce ', VINDI_IDENTIFIER); ?></strong>:
-            <?php printf(__('requer um certificado SSL para ativar este método de pagamento em modo de produção. Por favor, verifique se um certificado SSL está instalado em seu servidor !')); ?>
+            <?php printf(__('É necessário um <strong> Certificado SSL </strong> para ativar este método de pagamento em modo de produção. Por favor, verifique se um certificado SSL está instalado em seu servidor !')); ?> 
     </p>
 </div>
 <?php endif; ?>
