0f7af50096726d219166de771c20be5b490a92c7
Cristian Rossi <cristian@yogh.com.br>
2018-09-20 17:14:36 -0300

    Desabilita o handling de renewal orders pelo WC_Subscriptions para assinaturas do Vindi

--- vindi-woocommerce-subscriptions.php
+++ vindi-woocommerce-subscriptions.php
@@ -88,6 +88,7 @@ if (! class_exists('Vindi_WooCommerce_Subscriptions'))
     			'class-vindi-payment.php',
     			'class-vindi-webhook-handler.php',
     			'class-vindi-subscription-status-handler.php',
+				'class-vindi-wc-subscriptions-disable-renewal.php',
             ));
 
 			$this->settings                    = new Vindi_Settings();
