127b6a97f8b44e21792b89f76c8566b33c915d4f
Cristian Rossi <cristian@yogh.com.br>
2018-09-20 13:49:57 -0300

    Correção da ID da subscription para ativação da assinatura no Vindi e checagem de status 'on-hold'.

--- includes/class-vindi-subscription-status-handler.php
+++ includes/class-vindi-subscription-status-handler.php
@@ -17,21 +17,25 @@ class Vindi_Subscription_Status_Handler
 
         add_action('woocommerce_subscription_status_updated',array(
             &$this, 'filter_pre_status'
-        ), 1, 2);
+        ), 1, 3);
     }
 
     /**
      * @param WC_Subscription $wc_subscription
      * @param string          $new_status
      **/
-    public function filter_pre_status($wc_subscription, $new_status)
-    {   
+    public function filter_pre_status($wc_subscription, $new_status, $old_status)
+    {
+	    $vindi_subscription_id = $this->get_vindi_subscription_id( $wc_subscription );
+
         switch ($new_status) {
             case 'on-hold':
                 $this->suspend_status($this->get_vindi_subscription_id($wc_subscription));
                 break;
             case 'active':
-                $this->active_status($wc_subscription);
+            	if( 'on-hold' == $old_status ) {
+		            $this->active_status( $vindi_subscription_id );
+	            }
                 break;           
             default:
                 $this->cancelled_status($wc_subscription,$new_status);
@@ -61,12 +65,12 @@ class Vindi_Subscription_Status_Handler
     }
 
     /**
-     * @param WC_Subscription $wc_subscription
+     * @param string $vindi_subscription_id
      **/
-    public function active_status($wc_subscription)
+    public function active_status( $vindi_subscription_id )
     {
-        if ($wc_subscription->has_status('on-hold') && $this->container->get_synchronism_status()) {
-            $this->container->api->activate_subscription($this->vindi_subscription_id);
+        if ( $this->container->get_synchronism_status() ) {
+            $this->container->api->activate_subscription( $vindi_subscription_id );
         }
     }
 
