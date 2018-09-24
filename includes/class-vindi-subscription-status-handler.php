099f14b7a3d3c19843f0c070d26933c0e60be284
Cristian Rossi <cristian@yogh.com.br>
2018-09-24 09:51:45 -0300

    - Ajuste no filtro de pre_status para que qualquer status que não seja 'active' suspenda a assinatura. - Adicionado filtro para pre-processamento do status para que plugins ou tema possam customizar o funcionamento. - Removida restrição de 'on-hold' para reativar a assinatura. - Movida checagem de sincronia de status para o início da função simplificando o restante do código.

--- includes/class-vindi-subscription-status-handler.php
+++ includes/class-vindi-subscription-status-handler.php
@@ -1,81 +1,88 @@
 <?php
 
-class Vindi_Subscription_Status_Handler
-{
-    /**
-     * @var Vindi_Settings
-     **/
-    private $container;
-
-    public function __construct(Vindi_Settings $container)
-    {
-        $this->container = $container;
-
-        add_action('woocommerce_subscription_status_updated',array(
-            &$this, 'filter_pre_status'
-        ), 1, 3);
-    }
-
-    /**
-     * @param WC_Subscription $wc_subscription
-     * @param string          $new_status
-     * @param string          $old_status
-     **/
-    public function filter_pre_status($wc_subscription, $new_status, $old_status)
-    {
-	    $vindi_sub_id = $this->get_vindi_subscription_id( $wc_subscription );
-
-        switch ($new_status) {
-            case 'on-hold':
-                $this->suspend_status($this->get_vindi_subscription_id($wc_subscription));
-                break;
-            case 'active':
-            	if( 'on-hold' === $old_status ) {
-		            $this->active_status( $vindi_sub_id );
-	            }
-                break;
-	        case 'cancelled':
-		        $this->cancelled_status( $vindi_sub_id );
-	        	break;
-	        case 'pending-cancel':
-		        if ( ! $this->container->dependency->wc_memberships_are_activated() ) {
-			        $wc_subscription->update_status( 'cancelled' );
-		        }
-	        	break;
-        }
-    }
-
-    public function suspend_status($vindi_sub_id)
-    {
-        if ($this->container->get_synchronism_status()) {
-            $this->container->api->suspend_subscription($vindi_sub_id);
-        }
-    }
-
+class Vindi_Subscription_Status_Handler{
 	/**
-	 * @param string $vindi_subscription_id
+	 * @var Vindi_Settings
 	 **/
-	public function cancelled_status( $vindi_sub_id ) {
-		$this->container->api->suspend_subscription( $vindi_sub_id, true );
+	private $container;
+
+	public function __construct( Vindi_Settings $container ) {
+		$this->container = $container;
+
+		add_action( 'woocommerce_subscription_status_updated', array(
+			&$this,
+			'filter_pre_status'
+		), 1, 3 );
 	}
 
-    /**
-     * @param string $vindi_subscription_id
-     **/
-    public function active_status( $vindi_sub_id )
-    {
-        if ( $this->container->get_synchronism_status() ) {
-            $this->container->api->activate_subscription( $vindi_sub_id );
-        }
-    }
+	/**
+	 * @param WC_Subscription $wc_subscription
+	 * @param string          $new_status
+	 * @param string          $old_status
+	 **/
+	public function filter_pre_status( $wc_subscription, $new_status, $old_status ) {
 
-    /**
-     * @param WC_Subscription $wc_subscription
-     **/
-    public function get_vindi_subscription_id($wc_subscription)
-    {
-        $subscription_id = method_exists($wc_subscription, 'get_id') ? $wc_subscription->get_id() : $wc_subscription->id;
+		$subscription_id = $this->get_vindi_subscription_id( $wc_subscription );
 
-        return end(get_post_meta($subscription_id, 'vindi_wc_subscription_id'));
-    }
+
+		$result = apply_filters( 'vindi_status_handler_filter_pre_status', null, $wc_subscription, $new_status, $old_status, $this );
+
+		if ( null !== $result ) {
+			return;
+		}
+
+		if ( ! $this->container->get_synchronism_status() ) {
+			return;
+		}
+
+
+		switch ( $new_status ) {
+			case 'on-hold':
+				$this->suspend_status( $subscription_id );
+				break;
+			case 'active':
+				$this->active_status( $subscription_id );
+				break;
+			case 'pending-cancel':
+				if ( ! $this->container->dependency->wc_memberships_are_activated() ) {
+					$wc_subscription->update_status( 'cancelled' );
+				}
+				break;
+			default:
+				$this->suspend_status( $subscription_id );
+				break;
+		}
+
+	}
+
+	public function suspend_status( $subscription_id ) {
+		$this->container->api->suspend_subscription( $subscription_id );
+	}
+
+	/**
+	 * @param string $subscription_id
+	 **/
+	public function cancelled_status( $subscription_id ) {
+		$this->container->api->suspend_subscription( $subscription_id, true );
+	}
+
+	/**
+	 * @param string $subscription_id
+	 **/
+	public function active_status( $subscription_id ) {
+		$this->container->api->activate_subscription( $subscription_id );
+	}
+
+	/**
+	 * @param WC_Subscription $wc_subscription
+	 *
+	 * @return string vindi_subscription_ic
+	 **/
+	public function get_vindi_subscription_id( $wc_subscription ) {
+		$subscription_id = method_exists( $wc_subscription, 'get_id' )
+			? $wc_subscription->get_id()
+			: $wc_subscription->id;
+
+		return end( get_post_meta( $subscription_id, 'vindi_wc_subscription_id' ) );
+	}
 }
